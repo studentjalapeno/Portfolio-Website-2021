3e04ccd3d2b8d76af5816f802ac9bd6cf44955b9
studentjalapeno <60950099+studentjalapeno@users.noreply.github.com>
2021-08-25 17:43:19 -0500

    Added JS to homepage- buttons now route to new place

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get('/', function (req, res){
+   res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+
+app.listen(port);
+console.log('Server started at http://localhost' + port);
--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
 
+//Resume Click
+$('.home-options-resume').click(() => {
+    window.open("DevenMeza_Resume.pdf");
+    });
+
+//Contact Click
+$('.home-options-contact').click(() => {
+    window.open("contact.html");
+});
+
+//Tip Click
+$('.home-options-tip').click(() => {
+    window.open("https://www.paypal.com/paypalme/DevenMeza");
+});
+
+
+//Github Click
+$('.home-options-github').click(() => {
+    window.open("https://github.com/studentjalapeno");
+});
+
+//linkedin Click
+$('.home-options-linkedin').click(() => {
+    window.open("https://www.linkedin.com/in/deven-meza/");
+});
+
+//about Click
+$('.home-options-about').click(() => {
+    window.open("about.html");
+});
+
+
+//blog Click
+$('.home-options-blog').click(() => {
+    window.open("blog.html");
+});
+
+
